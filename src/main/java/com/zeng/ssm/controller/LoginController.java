e14b2c22cced52b9fcfe3e380c8d3dedde3fb9b6
xjl19951203 <761330163@qq.com>
2020-06-15 19:37:06 +0800

    20200615 注册和登录

--- src/main/java/com/zeng/ssm/controller/LoginController.java
+++ src/main/java/com/zeng/ssm/controller/LoginController.java
@@ -19,43 +19,64 @@ public class LoginController {
     @Resource
     UserDao userDao;
 
-    @RequestMapping(value = "/login", method = RequestMethod.POST)
-    public User register(@RequestBody String record) {
-//        System.out.println(record);
-        User temp = JSON.parseObject(record,User.class);
-//        System.out.println(temp.getUserName());
-        List<User> list = this.userDao.selectAll();
-        System.out.println(list);
-        for (User user:list) {
-            //邮箱或者用户名重复注册不成功
-          if ((user.getEmail().equals(temp.getEmail()))||(user.getUserName().equals(temp.getUserName()))) {
-//              System.out.println("0");
-              return null;
-          }
+    @RequestMapping(value = "/register", method = RequestMethod.POST)
+    public User register(@RequestBody User record) {
+        List<User> list = this.userDao.selectBySearch(record);
+        if (list.size()==0) {
+            this.userDao.insert(record);
+            return this.userDao.selectByPrimaryKey(record.getId());
         }
-//        System.out.println("1");
-        int pk = this.userDao.insert(temp);
-        return this.userDao.selectByPrimaryKey(pk);
+        System.out.println("0");
+        return null;
+
     }
 
-    @RequestMapping(value = "/login", method = RequestMethod.PUT)
-    public User login(@RequestBody String record) {
-        User temp = JSON.parseObject(record,User.class);
-        List<User> list = this.userDao.selectAll();
-        for (User user:list) {
-            if (user.getUserName().equals(temp.getUserName())) {
-                if (user.getPassword().equals(temp.getPassword())) {
-//                    System.out.println("yes");
-                    return user;//用户名存在且密码正确返回User对象
-                }else {
-//                    System.out.println("no");
-                    return temp;//用户名存在但密码错误，返回表单对象，此时邮箱为空
-                }
-            }
+    @RequestMapping(value = "/login", method = RequestMethod.POST)
+    public User login(@RequestBody User record) {
+        User user = this.userDao.selectByLogin(record);
+        if (user!=null) {
+            return user;
         }
-//        System.out.println("0");
+        System.out.println("0");
         return null;//用户名不存在，直接返回空值
     }
+//    @RequestMapping(value = "/register", method = RequestMethod.POST)
+//    public User register(@RequestBody String record) {
+////        System.out.println(record);
+//        User temp = JSON.parseObject(record,User.class);
+////        System.out.println(temp.getUserName());
+//        List<User> list = this.userDao.selectAll();
+//        System.out.println(list);
+//        for (User user:list) {
+//            //邮箱或者用户名重复注册不成功
+//          if ((user.getEmail().equals(temp.getEmail()))||(user.getUserName().equals(temp.getUserName()))) {
+////              System.out.println("0");
+//              return null;
+//          }
+//        }
+////        System.out.println("1");
+//        int pk = this.userDao.insert(temp);
+//        return this.userDao.selectByPrimaryKey(pk);
+//    }
+
+//    @RequestMapping(value = "/login", method = RequestMethod.POST)
+//    public User login(@RequestBody String record) {
+//        User temp = JSON.parseObject(record,User.class);
+//        List<User> list = this.userDao.selectAll();
+//        for (User user:list) {
+//            if (user.getUserName().equals(temp.getUserName())) {
+//                if (user.getPassword().equals(temp.getPassword())) {
+////                    System.out.println("yes");
+//                    return user;//用户名存在且密码正确返回User对象
+//                }else {
+////                    System.out.println("no");
+//                    return temp;//用户名存在但密码错误，返回表单对象，此时邮箱为空
+//                }
+//            }
+//        }
+////        System.out.println("0");
+//        return null;//用户名不存在，直接返回空值
+//    }
 
 //    @RequestMapping(value = "/login", method = RequestMethod.GET)
 //    public List<User> getLists() {
--- src/main/java/com/zeng/ssm/dao/UserDao.java
+++ src/main/java/com/zeng/ssm/dao/UserDao.java
@@ -2,13 +2,16 @@ package com.zeng.ssm.dao;
 
 import com.zeng.ssm.model.User;
 import org.apache.ibatis.annotations.Mapper;
-
 import java.util.List;
 
 @Mapper
 public interface UserDao {
 
     List<User> selectAll();
+
+    List<User> selectBySearch(User record);
+
+    User selectByLogin(User record);
 
     int insert(User record);
 
