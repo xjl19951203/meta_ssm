0a99ded2e04e43575944b804afffbfa3d196c0f0
xiangjilei <xjl19951203@163.com>
2020-06-19 13:53:31 +0800

    20200618

--- src/main/java/com/zeng/ssm/controller/BatchController.java
+++ src/main/java/com/zeng/ssm/controller/BatchController.java
@@ -27,6 +27,62 @@ public class BatchController {
     OutputPartDataDao outputPartDataDao;
 
     @RequestMapping(value="/sceneData", method = RequestMethod.POST)
+    public int sceneDataPost (@RequestBody SceneData record) {
+        try {
+            this.sceneDataDao.insert(record);
+            List<InputFrameData> inputFrameDatalist = record.getInputFrameDataList();
+            for (InputFrameData inputFrameData :inputFrameDatalist) {
+                inputFrameData.setSceneDataId(record.getId());
+                this.inputFrameDataDao.insert(inputFrameData);
+                List<MaterialData> materialDataList = inputFrameData.getMaterialDataList();
+                for (MaterialData materialData:materialDataList) {
+                    materialData.setInputFrameDataId(inputFrameData.getId());
+                    this.materialDataDao.insert(materialData);
+                }
+                List<EnergyData> energyDataList = inputFrameData.getEnergyDataList();
+                for (EnergyData energyData:energyDataList) {
+                    energyData.setInputFrameDataId(inputFrameData.getId());
+                    this.energyDataDao.insert(energyData);
+                }
+                List<DeviceData> deviceDataList = inputFrameData.getDeviceDataList();
+                for (DeviceData deviceData:deviceDataList) {
+                    deviceData.setInputFrameDataId(inputFrameData.getId());
+                    this.deviceDataDao.insert(deviceData);
+                }
+                List<KeyParameterData> keyParameterDataList = inputFrameData.getKeyParameterDataList();
+                for (KeyParameterData keyParameterData:keyParameterDataList) {
+                    keyParameterData.setInputFrameDataId(inputFrameData.getId());
+                    this.keyParameterDataDao.insert(keyParameterData);
+                }
+                List<FunctionUnitData> functionUnitDataList = inputFrameData.getFunctionUnitDataList();
+                for (FunctionUnitData functionUnitData:functionUnitDataList) {
+                    functionUnitData.setInputFrameDataId(inputFrameData.getId());
+                    this.functionUnitDataDao.insert(functionUnitData);
+                }
+                List<OutputFrameData> outputFrameDataList = inputFrameData.getOutputFrameDataList();
+                for (OutputFrameData outputFrameData:outputFrameDataList) {
+                    outputFrameData.setInputFrameDataId(inputFrameData.getId());
+                    this.outputFrameDataDao.insert(outputFrameData);
+                    List<EnvLoadData> envLoadDataList = outputFrameData.getEnvLoadDataList();
+                    for (EnvLoadData envLoadData:envLoadDataList) {
+                        envLoadData.setOutputFrameDataId(outputFrameData.getId());
+                        this.envLoadDataDao.insert(envLoadData);
+                    }
+                    List<OutputPartData> outputPartDataList = outputFrameData.getOutputPartDataList();
+                    for (OutputPartData outputPartData:outputPartDataList) {
+                        outputPartData.setOutputFrameDataId(outputFrameData.getId());
+                        this.outputPartDataDao.insert(outputPartData);
+                    }
+                }
+            }
+            return 1;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return 0;
+        }
+
+    }
+    @RequestMapping(value="/sceneDataList", method = RequestMethod.POST)
     public int sceneDataListPost (@RequestBody List<SceneData> record) {
         for (SceneData sceneData:record) {
             this.sceneDataDao.insert(sceneData);
@@ -58,7 +114,6 @@ public class BatchController {
 //                for (FunctionUnitData functionUnitData:functionUnitDataList) {
 //                    this.functionUnitDataDao.insert(functionUnitData);
 //                }
-
 
             }
 
